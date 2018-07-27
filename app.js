c365cdf5bb8c183901218fa0311a4b9ff932f4fd
李永轩 <lyx0717@yeah.net>
2018-07-27 16:37:06 +0800

    跟新版本1.0.3：增加call me按钮,优化，增加日历页

--- app.js
+++ app.js
@@ -1,30 +1,82 @@
 //app.js
+
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+    // 请求
+    // wx.request({
+    //   url: '',
+    //   data: {},
+    //   header: {
+    //     'content-type': 'application/json' // 默认值
+    //   },
+    //   method: 'GET', //（需大写）有效值：OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    //   dataType: 'json',
+    //   responseType: 'text',// 设置响应的数据类型。合法值：text、arraybuffer
+    //   success: function(res) {
+    //     console.log(res.data, '成功')
+    //   },
+    //   fail: function(res) {
+    //     console.log(res, '失败');
+    //   },
+    //   complete: function (res) {
+    //     console.log(res, 'complete');
+    //   }// 接口调用结束的回调函数（调用成功、失败都会执行）
+    // })
+
+    var that = this;
+    wx.login({
+      success: function(res){
+        var appid = "wxe0c55770c2186407";
+        var secret = "1e9b95e7242f4ec48526b91331bb49a0";
+        console.log(res);
+        if(res.code){
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          // wx.request({
+          //   url: 'url',
+          //   method: 'POST', // 必须大写
+          //   header: {
+          //     'content-type': 'application/json'
+          //   },
+          //   data: {
+          //     code: res.code
+          //   },
+          //   success: function(res){
+          //     console.log(res); // 拿到token
+          //     that.globalData.token = res.data.data.token;//拿到后将token存入全局变量  以便其他页面使用
+          //   }
+          // })
         }
-      })
-    }
+      }
+    })
   },
+
+// 获取用户信息（已废弃）
+  // getUserInfo:function(cb){
+  //   var that = this
+  //   if(this.globalData.userInfo){
+  //     typeof cb == "function" && cb(this.globalData.userInfo)
+  //   }else{
+  //     //调用登录接口
+  //     wx.login({
+  //       success: function () {
+  //         wx.getUserInfo({
+  //           success: function (res) {
+  //             that.globalData.userInfo = res.userInfo
+  //             typeof cb == "function" && cb(that.globalData.userInfo)
+  //           }
+  //         })
+  //       }
+  //     })
+  //   }
+  // },
   globalData:{
     userInfo: null
   }
 })
+
+// 获取token地址
+// https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxe0c55770c2186407&secret=6bc8dfb2a6030a72e27e06b9ea596a71
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,6 @@
-
 // pages/about/index.js
-var app = getApp()
+// var app = getApp()
+// var WXBizDataCrypt = require('../../utils/WXBizDataCrypt')/
 Page({
 
   /**
@@ -11,6 +11,7 @@ Page({
     userInfo: {},
     showViewFlag: false,
     userProfile:{
+      name:'李永轩',
       job:'前端开发工程师',
       location:'北京',
       profession:'IT开发',
@@ -18,7 +19,14 @@ Page({
       wechat:'LI919112965',
       mobile:'17607140609',
       github:'https://github.com/lyx0717',
-      introduction:'个人网名：李狗蛋，最喜欢的书是红楼梦，是资深红迷。'
+      introduction:'欢迎来到我的个人名片！'
+    },
+    mobileInfo: {
+      model:'', // 手机型号
+        pixelRatio: '', // 设备像素比
+        language: '', // 微信设置语言
+        version: '', // 微信版本
+        platform: '', // 客户端平台
     }
   },
 
@@ -26,19 +34,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      console.log(userInfo)
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-      that.setData({
-        showViewFlag: true
-      })
-    })
+    // console.log('onLoad')
+    // var that = this
+    // //调用应用实例的方法获取全局数据
+    // app.getUserInfo(function (userInfo) {
+    //   console.log(userInfo)
+    //   //更新数据
+    //   that.setData({
+    //     userInfo: userInfo
+    //   })
+    //   that.setData({
+    //     showViewFlag: true
+    //   })
+    // })
+    // this.getSprit();
+    this.getSystemInfo();
   },
   onShareAppMessage: function () {
     return {
@@ -52,6 +62,63 @@ Page({
       }
     }
   },
+
+  // 获取微信运动信息
+getSprit: function () {
+  wx.getWeRunData({
+    success(res) {
+      const encryptedData = res.encryptedData
+      // console.log('解密前 data: ', res)
+    },
+    fail: function (res) {
+      wx.showModal({
+        title: '提示',
+        content: '开发者未开通微信运动，请关注“微信运动”公众号后重试',
+        showCancel: false,
+        confirmText: '知道了'
+      })
+    }
+  })
+},
+
+// 获取设备信息
+getSystemInfo: function () {
+  try {
+    // 获取设备信息
+    wx.getSystemInfo({
+      success: function(res) {
+        // console.log(res, '设备')
+      }
+    })
+    wx.onCompassChange(function (res) {
+      // console.log(res.direction, '罗盘')
+    })
+    var res = wx.getSystemInfoSync()
+    // console.log(res);
+  } catch (e) {
+    // Do something when catch error
+  }
+},
+
+// 绑定input 
+bindKeyInput: function(e) {
+  this.setData({
+    inputValue: e.detail.value
+  })
+},
+
+// 打电话
+callPhone: function () {
+    wx.makePhoneCall({
+      phoneNumber: '17607140609', //仅为示例，并非真实的电话号码
+      success: function () {
+        console.log('打电话成功');
+      },
+      fail: function () {
+        console.log('打电话失败');
+      },
+    })
+},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
